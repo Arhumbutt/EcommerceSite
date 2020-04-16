2cdb1728a6d8fc1e0466802e9095a8036e93eddc
Hasnat Hameed <hasnat1508.22@gmail.com>
2020-04-16 11:47:04 +0500

    slider

--- src/app/productdetail/productdetail.component.ts
+++ src/app/productdetail/productdetail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-productdetail',
   templateUrl: './productdetail.component.html',
@@ -10,6 +10,17 @@ export class ProductdetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      nextArrow: '<i class="fa fa-angle-right slide-r"></i>',
+      prevArrow: '<i class="fa fa-angle-left slide-l"></i>'
+    });
+
+
+
+
   }
 
 }
